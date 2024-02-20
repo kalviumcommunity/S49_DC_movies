8c7fde5b19f6abb1d78783dfd7c61f7563fb83d1
Prithvi Kashyap <prithvi.kashyap1804@gmail.com>
2024-02-20 13:16:58 +0530

    update

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv'); 
-dotenv.config();
-
 const app = express();
 const port = 3000;
+const { MongoClient } = require("mongodb");
+app.use(express.json());
 
 
-const { connectToDB, disconnectFromDB, mongooseConnection } = require('./db');
-const config = { mongoURI: process.env.MONGO_URI }; 
 
+app.get('/ping',(req,res) => {
+  res.send("pong")
+}
+)
 
-connectToDB(config);
-
-app.get('/', (req, res) => {
-  const connectionStatus = mongooseConnection.readyState;
-  const message = connectionStatus === 1 ? 'Connected' : 'Not Connected';
-
-  res.send(`Database Connection Status: ${message}`);
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-app.get('/ping', (req, res) => {
-  res.send('pong');
+
+
+app.get("/", async (req, res) => {
+  try {
+    // Connect to the MongoDB database
+    await client.connect();
+
+    // Check if the connection is successful
+    if (client.topology.isConnected()) {
+      res.json({ message: "pong", database_status: "Connected" });
+      console.log("yes");
+    } else {
+      res.json({ message: "pong", database_status: "Disconnected" });
+      console.log("no");
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`🚀 server running on PORT: ${port}`);
   });
 }
+
 module.exports = app;
