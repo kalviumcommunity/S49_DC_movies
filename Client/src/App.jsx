a6e14d01c44f92fe1742fdf043ef9362fa3559dd
Prithvi Kashyap <prithvi.kashyap1804@gmail.com>
2024-02-26 15:52:52 +0530

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import './App.css';
+import Module11 from "./Component/module11"; 
 
-
-function App(){
-  return(
+function App() {
+  return (
     <div className="header">
-      <header>
+      
         <h1>DC Movies</h1>
-      </header>
+      
+      <div>
+        <Module11 /> 
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
